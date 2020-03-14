363c4badddf025ed1c3149bf63b9078c72b018d7
Luis Omar Emilio Perez Rodriguez <luis_perez6@ucne.edu.do>
2020-03-14 19:09:10 -0400

    Se instalaron y actualizaron los paquetes

---
+++ ProyectoFinalAplicada2/Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoFinalAplicada2.Models
+{
+    public class Cliente
+    {
+    }
+}
